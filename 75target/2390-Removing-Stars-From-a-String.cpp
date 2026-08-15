edf523cb5a45ad3e191d16d9c0ab0178aed35aee
Vaishnavi Sabale <145217729+sweenavi@users.noreply.github.com>
2026-08-15 22:56:51 +0530

    [2390] [Time Beats: 38.19%] [Memory Beats: 78.24%] - LeetPush

---
+++ 75target/2390-Removing-Stars-From-a-String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string st;
+        for(char c:s){
+            if(c=='*'){st.pop_back();}
+            else{st.push_back(c);}
+        }
+        return st;
+    }
+};
